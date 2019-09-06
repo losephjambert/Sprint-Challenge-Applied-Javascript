6fb211bd47fba89101128dad02f201e7cf1455b9
Joe <josephc.lambert@gmail.com>
2019-09-06 10:57:02 -0700

    carousel stretch complete

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -30,20 +30,17 @@ function Carousel(images) {
   const carousel = document.createElement('div');
   const leftButton = document.createElement('div');
   const rightButton = document.createElement('div');
-  const imgContainer = document.createElement('div');
-  const imagesArr = [...images, ...images].map(image => {
+  const imagesArr = images.map(image => {
     const newImg = document.createElement('img');
     newImg.src = image;
     return newImg;
   });
 
   carousel.classList.add('carousel');
-  imgContainer.classList.add('img-container');
   leftButton.classList.add('left-button');
   rightButton.classList.add('right-button');
 
-  imgContainer.append(...imagesArr);
-  carousel.append(leftButton, imgContainer, rightButton);
+  carousel.append(leftButton, ...imagesArr, rightButton);
 
   /* Functionality
     As a user:
@@ -68,7 +65,6 @@ function Carousel(images) {
 
   const handleCarouselTransition = (e, images) => {
     const previousImage = images[currentIndex];
-    let width = imgContainer.clientWidth;
 
     if (e.target == leftButton) {
       // move image to the right
@@ -76,8 +72,6 @@ function Carousel(images) {
       // slide new image in from left
       currentIndex -= 1;
       currentIndex = currentIndex < 0 ? imagesArr.length - 1 : currentIndex;
-      width += width;
-      imgContainer.style.transform = `translateX(${width}px)`;
     }
     if (e.target == rightButton) {
       // move image to the left
@@ -85,8 +79,6 @@ function Carousel(images) {
       // slide new image in from right
       currentIndex += 1;
       currentIndex = currentIndex > imagesArr.length - 1 ? 0 : currentIndex;
-      width -= width;
-      imgContainer.style.transform = `translateX(${-width}px)`;
     }
 
     const currentImage = images[currentIndex];
